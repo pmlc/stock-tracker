54598327cbcc0d2735a280fa810657b2f72dec0f
Pierre Le Count <pmlc@iinet.net.au>
2018-07-22 22:54:03 +0000

    Added gitignore

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('FinanceTrackerApp',[]); 
+var app = angular.module('FinanceTrackerApp',[])
+                .controller('strocksController',['$scope', function($scope){
+                
+                $scope.lookup = function(){
+                if($scope.ticker != undefined && $scope.ticker != '') {
+                    $scope.stock = {
+                         symbol:      'FOO',
+                         name:        'Example Corp.',
+                         last_price:  '123.00'
+                    }
+                    } else {
+                        $scope.stock = {};
+                            }
+                    }
+
+                 }])
+
+
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
+  attr_accessor :can_be_added
   
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
@@ -16,4 +17,7 @@ class Stock < ApplicationRecord
       return nil
     end
   end
+  
+  # stocks cannot be added as user already has 10 stocks
+  
 end
